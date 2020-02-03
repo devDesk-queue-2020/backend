077d6569cf30f0c89f6ba6088135e05a22f48c00
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2020-02-03 11:52:07 +0000

    Amend migration issues

--- knexfile.js
+++ knexfile.js
@@ -1,39 +1,41 @@
 // Update with your config settings.
+const dotenv = require("dotenv");
+dotenv.config();
 
 module.exports = {
   development: {
     client: "sqlite3",
     connection: {
-      filename: "./data/database/users"
+      filename: "./src/database/users.db3"
     },
     useNullAsDefault: true,
     migrations: {
-      directory: "./data/database/migrations"
+      directory: "./src/database/migrations"
     },
     seeds: {
-      directory: "./data/database/seeds"
+      directory: "./src/database/seeds"
     }
   },
   production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: "./data/database/migrations"
+      directory: "./src/database/migrations"
     },
-    seeds: { directory: "./data/database/seeds" }
+    seeds: { directory: "./src/database/seeds" }
   },
 
   testing: {
     client: "sqlite3",
     connection: {
-      filename: "./data/database/testing-users"
+      filename: "./src/database/testing-users.db3"
     },
     useNullAsDefault: true,
     migrations: {
-      directory: "./data/database/migrations"
+      directory: "./src/database/migrations"
     },
     seeds: {
-      directory: "./data/database/seeds"
+      directory: "./src/database/seeds"
     }
   }
 };
--- src/routers/index.js
+++ src/routers/index.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const userRouter = require("./users-router");
 const ticketRouter = require("./ticket-routers");
 
-router.use(userRouter);
-router.use(ticketRouter);
+router.use("/api/users", userRouter);
+router.use("/api/tickets", ticketRouter);
 
 module.exports = router;
