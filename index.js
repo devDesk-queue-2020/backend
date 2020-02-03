48977e1b268187677a7297f15739107c27c21eaf
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2020-02-03 10:58:23 +0000

    Amend script

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require("./server");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5200;
 
 server.listen(PORT, (req, res) => {
   console.log(`devDesk-2020 Server is listening on port: ${PORT}`);
