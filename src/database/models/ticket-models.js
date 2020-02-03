825fd73b5a48206ccdbcd05e517f4d524bd2ad6a
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2020-02-03 12:35:49 +0000

    Add getAll(), getById(), getByCat()

--- src/database/models/ticket-models.js
+++ src/database/models/ticket-models.js
@@ -1,9 +1,19 @@
 const db = require("../dbConfig");
 
 module.exports = {
-  getAllTickets
+  getAllTickets,
+  getTicketById,
+  getTicketByCategory
 };
 
 function getAllTickets() {
   return db("tickets");
+}
+
+function getTicketById(id) {
+  return db("tickets").where({ id });
+}
+
+function getTicketByCategory(category) {
+  return db("tickets").where({ category_id: category });
 }
