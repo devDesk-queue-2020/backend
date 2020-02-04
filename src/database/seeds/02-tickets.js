3d0cef0df864db3f9c9c736bfba8ed0c8c353c54
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2020-02-04 17:12:25 +0000

    Update seed tables to del() to enable deployment

--- src/database/seeds/02-tickets.js
+++ src/database/seeds/02-tickets.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex("tickets")
-    .truncate()
+    .del()
     .then(function() {
       // Inserts seed entries
       return knex("tickets").insert([
