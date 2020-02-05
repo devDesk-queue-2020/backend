553195614facef6286bc0591faac0a46e05d5d9d
niklasbec <niklasbeckerr@gmail.com>
2020-02-05 12:43:34 +0100

    testing done for user endpoints and model

--- src/database/models/tests/user-models.spec.js
+++ src/database/models/tests/user-models.spec.js
@@ -13,9 +13,29 @@ describe('Users model', () => {
       expect(returnData).length > 1
     })
 
+    it('returns a specific users correctly', async () => {
+      await Users.addUser({ first_name: "Niklas", last_name: "Becker", username: "Niklas", email: "email@email.com", role: "Student", password: "xee" })
+      const returnData = await Users.findUserById(1)
+      expect(returnData).toMatchObject({ first_name: "Niklas", last_name: "Becker", username: "Niklas", email: "email@email.com", role: "Student"})
+    })
+
     it('inserts the Users correctly', async () => {
-      const returnData = await Users.addUser({ first_name: "Nicki", last_name: "Becki", username: "Nicki", email: "email@email.com", role: "Student", password: "xee" })
-      expect(returnData).toMatchObject({ first_name: "Nicki", last_name: "Becki", username: "Nicki", email: "email@email.com", role: "Student" })
+      const returnData = await Users.addUser({ first_name: "Niklas", last_name: "Becker", username: "Niklas", email: "email@email.com", role: "Student", password: "xee" })
+      expect(returnData).toEqual([1])
+    })
+
+    it('deletes the Users correctly', async () => {
+      await Users.addUser({ first_name: "Niklas", last_name: "Becker", username: "Niklas", email: "email@email.com", role: "Student", password: "xee" })
+      const returnData = await Users.deleteUser(1)
+      expect(returnData).toEqual(1)
+    })
+
+    it('updates the Users correctly', async () => {
+      await Users.addUser({ first_name: "Niklas", last_name: "Becker", username: "Niklas", email: "email@email.com", role: "Student", password: "xee" })
+      await Users.updateUser(1, { first_name: "Jake", last_name: "Becker", username: "Niklas", email: "email@email.com", role: "Student", password: "xee" })
+      const data = await Users.findUserById(1) 
+      expect(data.first_name).toEqual('Jake')
+      expect(data.last_name).toEqual('Becker')
     })
   })
 })
